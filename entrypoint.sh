1b2a26aec56f6d1055958b018ebd90892fe7a4d6
Youssef Ahmed <youssef.a9300@gmail.com>
2023-11-07 15:41:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,1 @@
-
-	
-
 python3 $1
-
-#jmeter -n -t opensource-orangehrmlive.jmx \ -l /data/$SMOKE_RESULTS -j /data/$LOG_FILE
-	
-
-echo "--------------- execution done --------------"
-
-#cat $SMOKE_RESULTS
-
-echo "--------------- check Logs below --------------"
-
-#cat $LOG_FILE
-
-
-CMD ["python3", "main.py"]
